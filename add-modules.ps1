7933a7d4dc530da7b052f2484a516c14b517ba57
Tito Luyo Murata <tluyo@hotmail.com>
2022-11-04 13:28:07 -0500

    add modules

---
+++ add-modules.ps1
@@ -0,0 +1,6 @@
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+Install-Module -Name Az -Scope CurrentUser -Repository PSGallery -Force
+Install-Module -Name AzTable -Scope CurrentUser -Repository PSGallery -Force
+Install-Module -Name AzTable -Scope CurrentUser -Repository PSGallery -Force
+Install-Module -Name Az.Storage -Scope CurrentUser -Repository PSGallery -Force
+Install-Module -Name Microsoft.Online.SharePoint.Powershell -Scope CurrentUser -Repository PSGallery -Force
